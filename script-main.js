9498bf62d44ef3e758e2f36ae6b4316b77204a78
Eid-one <139388246+Eid-one@users.noreply.github.com>
2023-11-09 12:51:25 +0100

    Loop for Bigger
     This is  my First Loop Code  Days an Week

---
+++ script-main.js
@@ -0,0 +1,40 @@
+
+
+
+// for  lopp
+// While loop
+// Do While
+// for of loop
+// Break and Continue
+
+// nested Loop
+
+
+
+ let  days  = 7;
+ let kelendar =""
+
+  let week = 5;
+
+   for (let i = 1; i <= week; i++) {
+kelendar+=i;
+    console.log (" Week :" +i );
+
+     for (let j = 1; j <=days; j++) {
+
+       console.log ("Day :"+ j );
+      
+      
+
+        
+     }
+   
+    
+   }
+
+
+   
+
+
+
+
